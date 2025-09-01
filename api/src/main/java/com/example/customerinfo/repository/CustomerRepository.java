52a059d3ea8a4b211418938f324085e4e6763cef
cdw <cdwaty@gmail.com>
2025-09-01 22:36:17 +1200

    refactored SpringBoot app

--- api/src/main/java/com/example/customerinfo/repository/CustomerRepository.java
+++ api/src/main/java/com/example/customerinfo/repository/CustomerRepository.java
@@ -1,5 +1,6 @@
-package com.example.customerinfo;
+package com.example.customerinfo.repository;
 
+import com.example.customerinfo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
